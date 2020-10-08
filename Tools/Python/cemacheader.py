6d64cb93b633fca1d5203892e055788e896c14a6
Helen Burns <H.L.Burns@leeds.ac.uk>
2020-10-08 11:45:55 +0100

    :sparkles: intial cemac python template

---
+++ Tools/Python/cemacheader.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""cemacheader
+.. module:: cemacheader
+    :platform: Unix
+    :synopis: example header
+.. moduleauther: <name> @ CEMAC (UoL)
+.. description: This module was developed by CEMAC as part of the XXXX
+   Project. This Script does absolutely nothing.
+   :copyright: © 2020 University of Leeds.
+   :license: e.g. MIT/BSD-2/GPL 
+Example:
+    To use::
+        ./cemacheader.py <args>
+        <args> - arg info
+.. CEMAC_cemac_generic:
+   https://github.com/cemac/cemac_generic
+"""
